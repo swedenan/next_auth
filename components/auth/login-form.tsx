d743c89aa1365652b310154f77e84be2a215c75d
SwedeNaN <87441395+swedenan@users.noreply.github.com>
2024-03-17 00:13:18 +0300

    start form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,32 @@
-import { CardWrapper } from './card-wrapper'
+'use client'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import * as z from 'zod'
+
+import { LoginSchema } from '@/schemas'
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form'
+
+import { CardWrapper } from '@/components/auth/card-wrapper'
 
 export const LoginForm = () => {
+	const form = useForm<z.infer<typeof LoginSchema>>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	})
+
 	return (
 		<CardWrapper
 			headerLabel='Welcome back'
@@ -8,7 +34,9 @@ export const LoginForm = () => {
 			backButtonHref='/auth/register'
 			showSocial
 		>
-			LoginForm
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(() => {})}></form>
+			</Form>
 		</CardWrapper>
 	)
 }
